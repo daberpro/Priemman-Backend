f9f36c6650d55ab62e4d7b3a50c3c48e858bf8e7
daberpro <daber.coding@gmail.com>
2026-09-16 01:50:49 +0700

    Fix: fix some bug when user input the limit and offset more than int64

--- src/handlers/utils.hpp
+++ src/handlers/utils.hpp
@@ -7,6 +7,7 @@
 
 constexpr std::int64_t kDefaultPageSize = 20;
 constexpr std::int64_t kMaxPageSize = 50;
+constexpr std::size_t kMaxInt64Digits = 19;
 
 namespace priemman::utils {
 inline std::string BuildResult(std::string_view message) {
@@ -26,46 +27,61 @@ inline std::expected<int64_t, std::string> ParseInt(std::string_view value) {
   return result;
 }
 
+
 [[nodiscard("ParsePageSize return value cannot be ignored")]]
-inline std::int64_t ParsePageSize(const userver::server::http::HttpRequest &request) {
-  const auto arg = request.GetArg("page_size");
+inline std::int64_t ParsePageSize(
+    const userver::server::http::HttpRequest& request
+) {
+    const auto arg = request.GetArg("page_size");
 
-  if (arg.empty()) {
-    return kDefaultPageSize;
-  }
+    if (arg.empty() || arg.size() > kMaxInt64Digits) {
+        return kDefaultPageSize;
+    }
 
-  std::int64_t value{};
-  const auto [ptr, ec] =
-      std::from_chars(arg.data(), arg.data() + arg.size(), value);
+    std::int64_t value{};
 
-  if (ec != std::errc{} || ptr != arg.data() + arg.size()) {
-    return kDefaultPageSize;
-  }
+    const auto [ptr, ec] =
+        std::from_chars(
+            arg.data(),
+            arg.data() + arg.size(),
+            value
+        );
 
-  if (value <= 0) {
-    return kDefaultPageSize;
-  }
+    if (ec != std::errc{} || ptr != arg.data() + arg.size()) {
+        return kDefaultPageSize;
+    }
 
-  return value > kMaxPageSize ? kMaxPageSize : value;
+    if (value <= 0) {
+        return kDefaultPageSize;
+    }
+
+    return value > kMaxPageSize ? kMaxPageSize : value;
 }
 
 [[nodiscard("ParseOffset return value cannot be ignored")]]
-inline std::int64_t ParseOffset(const userver::server::http::HttpRequest &request) {
-  const auto token = request.GetArg("page_token");
+inline std::int64_t ParseOffset(
+    const userver::server::http::HttpRequest& request
+) {
+    const auto token = request.GetArg("page_token");
 
-  if (token.empty()) {
-    return 0;
-  }
+    if (token.empty() || token.size() > kMaxInt64Digits) {
+        return 0;
+    }
 
-  std::int64_t value{};
-  const auto [ptr, ec] =
-      std::from_chars(token.data(), token.data() + token.size(), value);
+    std::int64_t value{};
 
-  if (ec != std::errc{} || ptr != token.data() + token.size()) {
-    return 0;
-  }
+    const auto [ptr, ec] =
+        std::from_chars(
+            token.data(),
+            token.data() + token.size(),
+            value
+        );
 
-  return value < 0 ? 0 : value;
+    if (ec != std::errc{} || ptr != token.data() + token.size()) {
+        return 0;
+    }
+
+    return value < 0 ? 0 : value;
 }
 
 } // namespace priemman::utils
